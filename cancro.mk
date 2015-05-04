ab8f690cd63efa8a6367cda831d357c14758b9b6
Bitti09 <Bitti09@users.noreply.github.com>
2015-05-04 13:33:16 +0200

    set usb to mtp&adb

--- cancro.mk
+++ cancro.mk
@@ -15,7 +15,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp \
+    persist.sys.usb.config=mtp,adb \
     camera2.portability.force_api=1
 
 # Charger
