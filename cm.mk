f6a8ed9a509d9b30847bb56d3f1939db3cadad0b
MrBaNkS <banksmi09@gmail.com>
2015-05-17 17:29:06 -0500

    only one PRODUCT_BUILD_PROP_OVERRIDES is needed
    Change-Id: I952b541443e7331a053e91f518f39c61a2c2be08

--- cm.mk
+++ cm.mk
@@ -36,10 +36,7 @@ PRODUCT_MODEL := MI 3W
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=cancro PRODUCT_NAME=cancro
-
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
 
-## Use the latest approved GMS identifiers unless running a signed build
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/cancro/cancro:5.1.1/LMY47V/5.5.8:userdebug/test-keys PRIVATE_BUILD_DESC="cancro-userdebug 5.1.1 LMY47V 5.5.8 test-keys"
-
+# Build prop overrides
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE="cancro" PRODUCT_NAME="cancro" BUILD_FINGERPRINT="Xiaomi/cancro/cancro:5.1.1/LMY47V/5.5.8:userdebug/test-keys" PRIVATE_BUILD_DESC="cancro-userdebug 5.1.1 LMY47V 5.5.8 test-keys"
