9ad371698d5954acad2b185f277c7d4ff52b7ddc
MrBaNkS <banksmi09@gmail.com>
2015-08-18 23:47:30 -0500

    cm.mk: call aosp_base_telephony.mk instead of full_base_telephony

--- cm.mk
+++ cm.mk
@@ -17,8 +17,8 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
 
-# Inherit from those products. Most specific first
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit from the common open source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from cancro device
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
