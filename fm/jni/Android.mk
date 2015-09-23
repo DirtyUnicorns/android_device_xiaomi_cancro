1eee9441e739f315f59926b27e414b650382c9dc
MrBaNkS <banksmi09@gmail.com>
2015-09-23 10:01:13 -0500

    cancro: build fm radio from device tree
    Change-Id: I0bc46ac2550a486a76972999506b91a4a11e4add

---
+++ fm/jni/Android.mk
@@ -0,0 +1,27 @@
+ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+android_hardware_fm.cpp \
+ConfFileParser.cpp \
+ConfigFmThs.cpp \
+FmIoctlsInterface.cpp \
+FmPerformanceParams.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+        libnativehelper \
+        libcutils
+
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+LOCAL_MODULE := libqcomfm_jni
+LOCAL_MODULE_TAGS := optional
+
+ifeq ($(TARGET_QCOM_NO_FM_FIRMWARE),true)
+    LOCAL_CFLAGS += -DQCOM_NO_FM_FIRMWARE
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+endif # is-vendor-board-platform
